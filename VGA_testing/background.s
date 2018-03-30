068d6fe671ee8e01c6bd14fb767d90244bc04a1a
peterbelefteriadis <37524702+peterbelefteriadis@users.noreply.github.com>
2018-03-30 18:04:43 -0400

    Add files via upload

--- VGA_testing/background.s
+++ VGA_testing/background.s
@@ -1,6 +1,6 @@
 .equ ADDR_VGA, 0x08000000
-.equ OFFSET, 0x00000000
-
+.equ OFFSET, 0x0000008A
+.data
 .align 2
 # essentially behaves as though you had .byte .... [many many bytes declared]...
 BACKGROUND:
@@ -10,10 +10,7 @@ BACKGROUND:
 .global _start
 _start:
     movia r2,ADDR_VGA
-
-    movui r4,0xffff  /* White pixel */
-    sthio r4,1032(r2) /* pixel (4,1) is x*2 + y*1024 so (8 + 1024 = 1032) */
-
+	
 	movi r5, 240 # y max
     movi r6, 0 # y current
     movi r7, 320 # x max (added 1 to account for start at 1)
@@ -29,16 +26,18 @@ INNER:
 
     mov r9, r0
     add r9, r6, r8
+	slli r9, r9, 1
     addi r9, r9, OFFSET #location of pixel information from .bmp file
 
-    slli r10, r8, 2	#mulitply by 2
+    slli r10, r8, 1	#mulitply by 2
     slli r11, r6, 10 # multiply by 1024
     add r10, r10, r11
     movi r11, 0x8
     slli r11, r11, 24
     add r10, r10, r11
 
-	ldhuio r4, BACKGROUND(r9)
+	
+	ldhu r4, BACKGROUND(r9)
     sthio r4, 0(r10)
 
     br INNER
