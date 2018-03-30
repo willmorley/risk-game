014206dd3a0b09650d3ae2eca7fe0aee429adff9
Will Morley <will.morley@mail.utoronto.ca>
2018-03-29 21:36:08 -0400

    fixed for little endian

--- HEX.s
+++ HEX.s
@@ -8,22 +8,23 @@ DISPLAY:
     # r4 is mem addr of element to display
     # store registers to stack
     addi sp, sp, -24
-    stwio r16, 0(sp) # hex3-0 addr
-    stwio r17, 4(sp) 
-    stwio r18, 8(sp) # final converted value
-    stwio r19, 12(sp)
-    stwio r20, 16(sp)
-    stwio r21, 20(sp)
+    stw r16, 0(sp) # hex3-0 addr
+    stw r17, 4(sp) 
+    stw r18, 8(sp) # final converted value
+    stw r19, 12(sp)
+    stw r20, 16(sp)
+    stw r21, 20(sp)
     movia r16, HEX3_0_BASE
     # format values
 FIVE_FOUR:
-    ldwio r19, 0(r4) 
-    srli r20, r19, 24
+    ldw r19, 0(r4) 
+    srli r20, r19, 8 
     ldbu r21, HEX_PATTERNS(r20)
 
     slli r18, r21, 8
     
-    srli r20, r19, 16
+    #srli r20, r19, 16
+    andi r20, r19, 0b11111111
     mov r21, r0
     ldbu r21, HEX_PATTERNS(r20)
 
@@ -34,13 +35,13 @@ FIVE_FOUR:
 
 THREE_ZERO:
 
-    andi r20, r19, 0b11111111
+    srli r20, r19, 16
     ldbu r21, HEX_PATTERNS(r20)
     
     slli r18, r21, 24
     
-    ldwio r19, 4(r4)
-    ldwio r20, 0(r19)
+    ldw r19, 4(r4)
+    ldw r20, 0(r19)
     srli r20, r20, 24
     ldbu r21, HEX_PATTERNS(r20) 
 
@@ -48,22 +49,20 @@ THREE_ZERO:
 
     or r18, r21, r18
 
-    ldwio r19, 8(r4)
-    ldwio r20, 0(r19)
+    ldw r19, 8(r4)
+    ldw r20, 0(r19)
     srli r20, r20, 24
     ldbu r21, HEX_PATTERNS(r20) 
 
-    slli r21, r21, 16
+    slli r21, r21, 8 
     
     or r18, r18, r21
 
-    ldwio r19, 12(r4)
-    ldwio r20, 0(r19)
+    ldw r19, 12(r4)
+    ldw r20, 0(r19)
     srli r20, r20, 24
     ldbu r21, HEX_PATTERNS(r20) 
 
-    slli r21, r21, 16
-    
     or r18, r18, r21
 
 
@@ -72,12 +71,12 @@ THREE_ZERO:
     # Load to memory
 
     # restore registers from stack
-    ldwio r16, 0(sp)
-    ldwio r17, 4(sp)
-    ldwio r18, 8(sp)
-    ldwio r19, 12(sp)
-    ldwio r20, 16(sp)
-    ldwio r21, 20(sp)
+    ldw r16, 0(sp)
+    ldw r17, 4(sp)
+    ldw r18, 8(sp)
+    ldw r19, 12(sp)
+    ldw r20, 16(sp)
+    ldw r21, 20(sp)
     addi sp, sp, 24
 ret    
 
--- main.s
+++ main.s
@@ -1,9 +1,9 @@
-.text
+.section .text
 .global _start
 
 _start:
     movia r4, T_1
-    addi sp, r0, 0x10000
+    addi sp, r0, 0x1000
     call DISPLAY
 END:
     br END
