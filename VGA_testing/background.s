2c16cb9c9ee3ea9877bb10f7d6c78b85c82675b1
peterbelefteriadis <37524702+peterbelefteriadis@users.noreply.github.com>
2018-03-30 18:51:55 -0400

    Add files via upload

--- VGA_testing/background.s
+++ VGA_testing/background.s
@@ -1,34 +1,41 @@
 .equ ADDR_VGA, 0x08000000
 .equ OFFSET, 0x0000008A
-.data
 .align 2
+
+.section .data
 # essentially behaves as though you had .byte .... [many many bytes declared]...
 BACKGROUND:
     .incbin "background.bmp"
 
-.text
+.section .text
 .global _start
 _start:
     movia r2,ADDR_VGA
 	
 	movi r5, 240 # y max
-    movi r6, 0 # y current
+    movi r6, -1 # y current
     movi r7, 320 # x max (added 1 to account for start at 1)
-    movi r8, 0 # x current
-
+    movi r8, -1 # x current
+	movi r12, OFFSET
+	addi r12, r12, 30720
+	addi r12, r12, 30720
+	addi r12, r12, 30720
+	addi r12, r12, 30720
+	addi r12, r12, 30720
 OUTER:
 	bge r6, r5, END
 	addi r6, r6, 1
-    movi r8, 0
+    movi r8, -1
 INNER:
 	bge r8, r7, OUTER
     addi r8, r8, 1
-
+	addi r12, r12, -2 
+/*
     mov r9, r0
     add r9, r6, r8
 	slli r9, r9, 1
     addi r9, r9, OFFSET #location of pixel information from .bmp file
-
+*/
     slli r10, r8, 1	#mulitply by 2
     slli r11, r6, 10 # multiply by 1024
     add r10, r10, r11
@@ -36,8 +43,7 @@ INNER:
     slli r11, r11, 24
     add r10, r10, r11
 
-	
-	ldhu r4, BACKGROUND(r9)
+	ldhu r4, BACKGROUND(r12)
     sthio r4, 0(r10)
 
     br INNER
